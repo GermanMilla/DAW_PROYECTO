79bc908c1674605caa73429fa77f0dc1b68e1947
GermanMilla <pymosv@gmail.com>
2021-11-23 21:23:06 -0600

    presentacion de proyecto

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+localStorage.username = "Ash Ketchum";
+localStorage.pin = "1234";
+localStorage.balance = 500;
+localStorage.account = 0987654321;
 
-    var username = {
-        name : "Ash Ketchum",
-        pin : 1234,
-        balance : (500).toPrecision(2),
-        account : 0987654321
-    };
 
-document.getElementById("usuario").innerHTML = "Hola, " + username.name + ". Cuenta no. " + username.account;
+
+document.getElementById("usuario").innerHTML = "Hola, " + localStorage.username + ". Cuenta no. " + localStorage.account;
+document.getElementById("saldo").innerHTML = localStorage.username + ", su saldo es de: $" + localStorage.balance;
+
+//funcion que valida el pin
+function validar() {
+    var pin = document.getElementById("pin").value;
+    if(pin == localStorage.pin){
+        document.location='index.html';
+    }else{
+        swal("Pin incorrecto", "", "error");
+        // document.location='splash.html';
+    }
+}
