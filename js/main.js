6e1c296e5dd09db831608e2d02813fceb256ffd1
GermanMilla <pymosv@gmail.com>
2021-11-28 12:58:16 -0600

    agregadas funciones de retiro y deposito de fondos

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-localStorage.username = "Ash Ketchum";
-localStorage.pin = "1234";
-localStorage.balance = 500;
-localStorage.account = 0987654321;
+//variables globales
+document.getElementById("usuario").innerHTML = "Hola, " + localStorage.username + ". Cuenta no. " + localStorage.getItem("account");
+if (!pagoselected > 0){
+    pagoselected = 0;
+}
 
+function iniciar(){
+    localStorage.pin = "1234";
 
-document.getElementById("usuario").innerHTML = "Hola, " + localStorage.username + ". Cuenta no. " + localStorage.account;
-document.getElementById("saldo").innerHTML = localStorage.username + ", su saldo es de: $" + localStorage.balance;
+    localStorage.setItem("username", "Ash Ketchum");
+    localStorage.setItem("balance", 500);
+    localStorage.setItem("account", 0987654321);
+}
+
+function consultarusuario(){
+    document.getElementById("saldo").innerHTML = localStorage.getItem("username") + ", su saldo es de: $" + localStorage.getItem("balance");
+}
 
 //funcion que valida el pin
 function validar() {
     var pin = document.getElementById("pin").value;
     if(pin == localStorage.pin){
         document.location='index.html';
+        iniciar();
     }else{
         swal("Pin incorrecto", "", "error");
         // document.location='splash.html';
@@ -23,10 +33,47 @@ function salir(){
     document.location='splash.html';
 }
 
-
 function deposito(){
-    var cantidad = document.getElementById("cantidad").value 
-    localStorage.balance = localStorage.balance * 1 + cantidad * 1;
-    console.log(localStorage.balance);
+    var cantidad = document.getElementById("cantidad").value;
+    var balance = localStorage.getItem("balance");
+    var nuevobalance = parseFloat(cantidad) + parseFloat(balance);
+    localStorage.setItem("balance", nuevobalance);
+    console.log(localStorage.getItem("balance"));
     document.location='index.html';
+}
+
+function retiro(){
+    var cantidad = document.getElementById("cantidadretiro").value;
+    var balance = localStorage.getItem("balance");
+    if (parseFloat(balance) -  parseFloat(cantidad) < 0){
+        swal("Fondos insuficientes" , "Por favor intente de nuevo","error");
+    } else{
+        var nuevobalance = parseFloat(balance) - parseFloat(cantidad);
+        localStorage.setItem("balance", nuevobalance);
+        console.log(localStorage.getItem("balance"));
+        document.location='resultadoretiro.html';
+
+    }   
+}
+
+function pagoServicio(pago){
+    switch(pago){
+        case 1:
+            pagoselected = 1;
+            break;
+        case 2:
+            pagoselected = 2;
+            break;
+        case 3:
+            pagoselected = 3;
+            break;
+        case 4:
+            pagoselected = 4;
+            break;
+    }
+    document.location="cobroservicio.html";
+}
+
+function inicializarpago(){
+    pagoselected = 0;
 }
